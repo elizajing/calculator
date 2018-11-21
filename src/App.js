b00fd24fa3219c7067767110b9d40837a0f648b5
Eliza Jing <elizajing@Elizas-MacBook-Pro-2.local>
2018-11-21 15:55:11 +0100

    Add black layer on gray background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+// import styles from './App.css';
 import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.background}>
-
+        
+        <div className={styles.blackbackground}></div>
       </div>
     );
   }
