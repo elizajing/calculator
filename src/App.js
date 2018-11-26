d34a8acda9c561d4497d3adb408cd8ed3c632333
Eliza Jing <elizajing@Elizas-MacBook-Pro-2.local>
2018-11-26 16:12:32 +0100

    Change symbol buttons to yellow

--- src/App.js
+++ src/App.js
@@ -12,52 +12,52 @@ class App extends Component {
           </div>
           <div className={styles.buttonboard}>
             <div className={styles.buttonzero}>
-            <p>0</p>
+              <p>0</p>
+            </div>
+            <div className={styles.symbolbutton}>
+              <p>.</p>
+            </div>
+            <div className={styles.symbolbutton}>
+              <p>+</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>.</p>
+              <p>1</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>+</p>
+              <p>2</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>1</p>
+              <p>3</p>
+            </div>
+            <div className={styles.symbolbutton}>
+              <p>-</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>2</p>
+              <p>4</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>3</p>
+              <p>5</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>-</p>
+              <p>6</p>
+            </div>
+            <div className={styles.symbolbutton}>
+              <p>x</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>4</p>
+              <p>7</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>5</p>
+              <p>8</p>
             </div>
             <div className={styles.regularbutton}>
-            <p>6</p>
+              <p>9</p>
             </div>
-            <div className={styles.regularbutton}>
-            <p>x</p>
-            </div>
-            <div className={styles.regularbutton}>
-            <p>7</p>
-            </div>
-            <div className={styles.regularbutton}>
-            <p>8</p>
-            </div>
-            <div className={styles.regularbutton}>
-            <p>9</p>
-            </div>
-            <div className={styles.regularbutton}>
-            <p>/</p>
+            <div className={styles.symbolbutton}>
+              <p>/</p>
             </div>
             <div className={styles.acbutton}>
-            <p>AC</p>
+              <p>AC</p>
             </div>
           </div>
         </div>
