ac062471421a98c8dc8e4868ad14c3d73cbfb95b
Eliza Jing <elizajing@Elizas-MacBook-Pro-2.local>
2018-11-26 14:24:06 +0100

    Add remaining css for the overall layout

--- src/App.js
+++ src/App.js
@@ -10,6 +10,56 @@ class App extends Component {
         <div className={styles.blackbackground}>
           <div className={styles.screen}>
           </div>
+          <div className={styles.buttonboard}>
+            <div className={styles.buttonzero}>
+            <p>0</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>.</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>+</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>1</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>2</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>3</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>-</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>4</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>5</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>6</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>x</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>7</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>8</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>9</p>
+            </div>
+            <div className={styles.regularbutton}>
+            <p>/</p>
+            </div>
+            <div className={styles.acbutton}>
+            <p>AC</p>
+            </div>
+          </div>
         </div>
       </div>
     );
