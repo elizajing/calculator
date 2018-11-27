f22df04bf518fe2b132b04eb2a60508808eb49e1
Eliza Jing <elizajing@Elizas-MBP-2.localdomain>
2018-11-27 13:31:49 +0100

    Slim down css code and rename classes

--- src/App.js
+++ src/App.js
@@ -7,57 +7,60 @@ class App extends Component {
   render() {
     return (
       <div className={styles.background}>
-        <div className={styles.blackbackground}>
+        <div className={styles.blackBackground}>
           <div className={styles.screen}>
           </div>
-          <div className={styles.buttonboard}>
-            <div className={styles.buttonzero}>
+          <div className={styles.buttonBoard}>
+            <div className={styles.buttonZero}>
               <h4>0</h4>
             </div>
-            <div className={styles.symbolbutton}>
+            <div className={styles.symbolButton}>
               <h4>.</h4>
             </div>
-            <div className={styles.symbolbutton}>
+            <div className={styles.symbolButton}>
               <h4>+</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>1</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>2</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>3</h4>
             </div>
-            <div className={styles.symbolbutton}>
+            <div className={styles.symbolButton}>
               <h4>-</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>4</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>5</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>6</h4>
             </div>
-            <div className={styles.symbolbutton}>
+            <div className={styles.symbolButton}>
               <h4>x</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>7</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>8</h4>
             </div>
-            <div className={styles.regularbutton}>
+            <div className={styles.regularButton}>
               <h4>9</h4>
             </div>
-            <div className={styles.symbolbutton}>
+            <div className={styles.symbolButton}>
               <h4>/</h4>
             </div>
-            <div className={styles.acbutton}>
+            <div className={styles.acButton}>
               <h4>AC</h4>
+            </div>
+            <div className={styles.symbolButton}>
+              <h4>=</h4>
             </div>
           </div>
         </div>
