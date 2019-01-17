b9bf957aa752713d9f2f059b90f8361222a26fa7
Eliza Jing <elizajing@MacBook-Pro.localdomain>
2019-01-17 13:26:45 +0100

    Change from .css to .scss

---
+++ src/Screen/screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Screen extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+export default Screen;
